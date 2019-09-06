554cc0044dcfd2d0ac5da13786ba3aeecbcb6ad6
Emily Lillie <emilylillie3@gmail.com>
2019-09-06 00:26:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,17 +11,14 @@ module FormsLab
       erb :'pirates/new'
     end
 
-     post '/pirates' do 
+    post '/pirates' do 
       @pirate = Pirate.new(params[:pirate])
-
-       params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-
-       @ships = Ship.all
-
-       erb :'pirates/show'
-    end
+      params[:pirate][:ships].each do |ship_details|
+        Ship.new(ship_details)
+      end 
+      @ships = Ship.all
+      erb :'pirates/show'
+    end 
 
   end
 end
